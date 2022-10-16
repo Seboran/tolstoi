8294119eda107258ea78c122e03bf76bb1b91f76
Nirina Rabeson <nirinarabeson@outlook.fr>
2022-10-16 22:48:44 +0200

    init

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts"></script>
+
+<template>
+  <header>Yo</header>
+  <main>Yo angelo</main>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+header {
+  display: flex;
+  place-items: center;
+  padding-right: calc(var(--section-gap) / 2);
+}
+
+.logo {
+  margin: 0 2rem 0 0;
+}
+</style>
