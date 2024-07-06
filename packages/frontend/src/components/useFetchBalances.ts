63dc39873c04d3447dbbfe589af738937137b298
Nirina Rabeson <nirina.rabeson@zenika.com>
2024-07-06 22:01:41 +0200

    feat: permet de déployer sur vercel

--- packages/frontend/src/components/useFetchBalances.ts
+++ packages/frontend/src/components/useFetchBalances.ts
@@ -4,7 +4,7 @@ interface BalanceSolutionResponse {
   result_matrix: number[][]
 }
 
-const BASE_URL = import.meta.env.PROD ? import.meta.env.VITE_BACKEND_URL : 'http://localhost:5000'
+const BASE_URL = import.meta.env.PROD ? import.meta.env.VITE_BACKEND_URL : 'http://localhost:3000'
 
 export async function fetchBalances(balances: MaybeRef<number[]>) {
   const response = await fetch(`${BASE_URL}/solve`, {
