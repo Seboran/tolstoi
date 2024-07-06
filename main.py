77cd65a7452ef149bbf653cf5a70f391d44db3e6
Nirina Rabeson <nirina.rabeson@zenika.com>
2024-07-06 09:15:04 +0200

    tests: recherche des solutions mal définies...

--- main.py
+++ main.py
@@ -1,50 +1,38 @@
 import scipy.optimize as opt
 import numpy as np
+import time
 
 
 def solve_for_n(balances: list):
     n = len(balances)
 
     def constraint1(vec):
-        # Transform 9x1 vector into 3x3 matrix
         vec = np.reshape(vec, (n, n))
-        # Define a second vector
         vecteur_uns = np.ones((n))
 
-        # Compute the dot product of vector1 and vector2
         product = np.dot(vec, vecteur_uns)
 
-        return (product - np.array(balances))
+        return product - np.array(balances)
 
     def constraint_anti_symmetric(vec):
-        # Reshape vec into a 3x3 matrix
         matrix = np.reshape(vec, (n, n))
-        # Calculate the difference between matrix and its negative transpose
         diff = matrix + matrix.T
-        # Flatten the difference matrix to a vector and return its L2 norm
         return np.linalg.norm(diff.flatten(), ord=2)
 
     def minimize_vector(vector1):
-        # Return the result
-        return np.sum(np.abs(vector1))
+        return np.linalg.norm(vector1, ord=1)
 
-    # Use scipy.optimize.minimize to find the minimum of the function
-    somme_initiale_a_transferer = sum(
-        filter(lambda x: x > 0, BALANCES_6_PERSONNES))
     initial_guess = np.zeros(n**2)
 
-    print("somme_initiale_a_transferer", somme_initiale_a_transferer)
-    result = opt.minimize(minimize_vector, x0=initial_guess, constraints=[
-        {'type': 'eq', 'fun': constraint1},  # First constraint
-        # Second constraint ensuring diagonal terms are zero
+    result = opt.minimize(minimize_vector, x0=initial_guess, method="SLSQP", constraints=[
+        {'type': 'eq', 'fun': constraint1},
         {'type': 'eq', 'fun': constraint_anti_symmetric},
-    ], options={'disp': True, 'maxiter': 1000})
+    ], options={'maxiter': 1000})
 
     argument_result = np.reshape(result.x, (n, n))
     argument_result = np.round(argument_result, 2)
-    # Print the minimum value and the corresponding argument
-    print("Minimum value:", result.fun / 2)
-    print("Argument that minimizes the function:\n", argument_result)
+
+    return (argument_result, result.fun, result.message, result.success)
 
 
 def ajouter_depense(depense: int, index_receveur: int, balances: list):
@@ -55,12 +43,61 @@ def ajouter_depense(depense: int, index_receveur: int, balances: list):
     return nouvelles_balances
 
 
-BALANCES_6_PERSONNES = np.zeros(5)
-for count in np.random.randint(1, 200, 150):
-    BALANCES_6_PERSONNES = ajouter_depense(
-        count, np.random.randint(0, len(BALANCES_6_PERSONNES)), BALANCES_6_PERSONNES)
+def ajouter_depense_avec_trous(depense: int, index_receveur: int, balances: list):
+    nombre_rembourseurs = len(balances)
 
-print(BALANCES_6_PERSONNES)
-print("somme totale", sum(filter(lambda x: x > 0, BALANCES_6_PERSONNES)))
+    nouvelles_balances = balances.copy()
+    choices = np.random.choice(nombre_rembourseurs, np.random.randint(
+        2, nombre_rembourseurs), replace=False)
 
-solve_for_n(BALANCES_6_PERSONNES)
+    for choice in choices:
+        nouvelles_balances[choice] -= depense / len(choices)
+
+    nouvelles_balances[index_receveur] += depense
+    return nouvelles_balances
+
+
+def test_valeurs():
+    while True:
+
+        NOMBRE_GENS = 7
+        BALANCES = np.zeros(NOMBRE_GENS)
+        for count in np.random.randint(1, 60, 1500):
+            random_spender = np.random.randint(0, len(BALANCES))
+            BALANCES = ajouter_depense_avec_trous(
+                count, random_spender, BALANCES)
+
+        BALANCES = np.round(BALANCES, 2)
+        start_time = time.time()
+        calculate_reimbursements(BALANCES)
+
+        elapsed_time = time.time() - start_time
+        # print("Elapsed time:", elapsed_time)
+
+
+def calculate_reimbursements(BALANCES):
+    nouvelles_balances = BALANCES.copy()
+    nouvelles_balances[0] -= sum(nouvelles_balances)
+    (matrice_de_remboursements, solution, message,
+     success) = solve_for_n(nouvelles_balances)
+
+    if not success:
+        print(BALANCES, sum(BALANCES))
+        print(np.round(matrice_de_remboursements, 2))
+        print("Error from solution", np.abs(
+            solution / 2 - sum(filter(lambda x: x > 0, BALANCES))))
+        print(message)
+        print()
+
+
+def calculate_hfti_example():
+    hfti_balances_example = [-1366.71666667,  194.51666667, 182.83333333,  628.55,
+                             215.61666667,  46.78333333,   98.41666667]
+
+    calculate_reimbursements(
+        hfti_balances_example)
+
+
+test_valeurs()
+
+# calculate_hfti_example()
