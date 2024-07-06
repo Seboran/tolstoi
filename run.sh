b6bd47ff74866ae57ba84aed0a15938b561b15ef
Nirina Rabeson <nirina.rabeson@zenika.com>
2024-07-06 18:33:02 +0200

    feat: adds vue app

--- run.sh
+++ run.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
+# Run Python application in the background
 export FLASK_APP=app.py
-python app.py
+python packages/backend/app.py &
+
+# Run JavaScript application in the background
+cd packages/frontend
+bun dev &
+cd -
+
+# Wait for both applications to finish
+wait
