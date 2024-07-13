0b1a1591af5ac5078060c72bdef138daf3f4d436
Nirina Rabeson <nirina.rabeson@zenika.com>
2024-07-13 11:52:32 +0200

    chore: change baseURL

--- tests/golden_tests.spec.ts
+++ tests/golden_tests.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test'
 
 test('test', async ({ page }) => {
-  await page.goto('http://localhost:3000/')
+  await page.goto('/')
   await page.getByRole('button', { name: 'Ajouter une personne' }).click()
   await page.getByRole('button', { name: 'Ajouter une personne' }).click()
   await page.getByRole('button', { name: 'Ajouter une personne' }).click()
