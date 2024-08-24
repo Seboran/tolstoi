b683f471210cc67bbc7fad660c31a8aa3fbdfc9a
Nirina Rabeson <hello@nirinarabeson.fr>
2024-08-24 12:47:44 +0200

    fix: pourquoi mes routes n'apparaissent pas ? :'(

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/',
   plugins: [VueRouter(), vue(), vueDevTools()],
   resolve: {
     alias: {
