419f34535909dece9b512a7c505f76a56ecc8395
Nirina Rabeson <hello@nirinarabeson.fr>
2024-08-25 22:59:11 +0200

    fix: met un vrai linter

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,12 +4,20 @@ require('@rushstack/eslint-patch/modern-module-resolution')
 module.exports = {
   root: true,
   extends: [
-    'plugin:vue/vue3-recommanded',
+    'plugin:vue/vue3-essential',
     'eslint:recommended',
     '@vue/eslint-config-typescript',
     '@vue/eslint-config-prettier/skip-formatting'
   ],
   parserOptions: {
     ecmaVersion: 'latest'
-  }
+  },
+  overrides: [
+    {
+      files: ['src/pages/*.vue'], // Change this to default.vue path
+      rules: {
+        'vue/multi-word-component-names': 'off'
+      }
+    }
+  ]
 }
