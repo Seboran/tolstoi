dd23094ec2441a7590ce209123b2c80c7d53c30a
Nirina Rabeson <hello@nirinarabeson.fr>
2024-09-22 19:00:41 +0200

    fix: ajout code source
    on est GPL maintenant ^^

--- src/App.vue
+++ src/App.vue
@@ -9,11 +9,25 @@ import BalancesFormV2 from './components/v2/BalancesFormV2.vue'
   <main>
     <BalancesFormV2 />
   </main>
+  <footer>
+    <a href="https://github.com/Seboran/bons-comptes-bons-amis"> Code github </a>
+  </footer>
 </template>
 
 <style scoped>
 header {
   margin: auto;
   text-align: center;
+}
+
+main {
+  min-height: 80vh;
+}
+
+footer {
+  position: relative;
+  right: 20px;
+  bottom: 10px;
+  text-align: right;
 }
 </style>
