fea60976f3319b80869a08c412cb9192d53c6ff4
Nirina Rabeson <hello@nirinarabeson.fr>
2024-09-28 13:59:09 +0200

    playwright?

---
+++ tests/test.spec.ts
@@ -0,0 +1,38 @@
+import { test, expect } from '@playwright/test'
+
+test('has title', async ({ page }) => {
+  await page.goto('/')
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Nirina Rabeson/)
+})
+
+test('get started link', async ({ page, context }) => {
+  await page.goto('/')
+
+  const pagePromise = context.waitForEvent('page')
+
+  // Click the get started link.
+  await page.getByRole('link', { name: 'Me contacter' }).click()
+
+  const newPage = await pagePromise
+
+  // Expects page to have a heading with the name of Installation.
+  await expect(newPage).toHaveURL('https://www.linkedin.com/in/nirinarabeson/')
+})
+
+test("affiche le titre d'un article et permet de cliquer dessus", async ({
+  page
+}) => {
+  await page.goto('/')
+  await page.getByRole('heading').getByRole('link').first().click()
+  const textContent = await page.getByRole('heading').nth(1).textContent()
+
+  await expect(page).toHaveURL(/\/posts\/*/)
+  await expect(page).toHaveTitle(new RegExp(textContent!))
+})
+
+test('affiche 404', async ({ page }) => {
+  await page.goto('/jemesuisperduetjedevraispasetreici2388394')
+  await expect(page.getByText('404 Page Not Found')).toBeVisible()
+})
