e69ba98063b75760ec1da9aa995e5c4a40ae8a89
Nirina Rabeson <hello@nirinarabeson.fr>
2024-09-29 14:13:18 +0000

    un peu plus propre

--- .vitepress/theme/Layout.vue
+++ .vitepress/theme/Layout.vue
@@ -1,54 +1,21 @@
 <script setup lang="ts">
 import { useData } from 'vitepress'
-import { onMounted, ref } from 'vue'
 import Article from './Article.vue'
 import Contact from './Contact.vue'
 import Home from './Home.vue'
 import NotFound from './NotFound.vue'
+import AnimatedBoutonMeContacter from './components/AnimatedBoutonMeContacter.vue'
+import AnimatedTitle from './components/AnimatedTitle.vue'
 
 const { page, frontmatter } = useData()
-
-const shake = ref(false)
-
-const DELAI_AVANT_AGITER_BOUTON_CONTACTER_IN_MS = 5000
-onMounted(() => {
-  setTimeout(
-    () => (shake.value = true),
-    DELAI_AVANT_AGITER_BOUTON_CONTACTER_IN_MS
-  )
-})
 </script>
 
 <template>
   <div class="antialiased dark:bg-slate-900">
     <div class="max-w-3xl mx-auto px-4 sm:px-6 xl:max-w-5xl xl:px-0">
       <nav class="flex justify-between items-center py-10 font-bold">
-        <a class="text-xl" href="/" aria-label="The Vue Point">
-          ✨
-          <Transition name="title-apparaitre">
-            <span
-              v-if="!frontmatter.index"
-              class="hidden md:inline dark:text-white"
-              >Nirina Rabeson</span
-            >
-          </Transition>
-        </a>
-        <Transition appear name="appear">
-          <div
-            class="text-sm text-gray-500 dark:text-white leading-5"
-            :class="{
-              shake
-            }"
-          >
-            <a
-              class="hover:text-gray-700 dark:hover:text-gray-200"
-              href="https://www.linkedin.com/in/nirinarabeson/"
-              target="_blank"
-              rel="noopener"
-              >Me contacter →</a
-            >
-          </div>
-        </Transition>
+        <AnimatedTitle :show="frontmatter.index" />
+        <AnimatedBoutonMeContacter />
       </nav>
     </div>
     <main class="max-w-3xl mx-auto px-4 sm:px-6 xl:max-w-5xl xl:px-0">
@@ -59,51 +26,3 @@ onMounted(() => {
     </main>
   </div>
 </template>
-
-<style lang="css" scoped>
-.title-apparaitre-enter-active {
-  transition: opacity 0.5s ease;
-}
-
-.title-apparaitre-enter-from {
-  opacity: 0;
-}
-.appear-enter-active,
-.appear-leave-active {
-  transition: opacity 0.5s ease;
-  transition-delay: 0.225s;
-}
-
-.appear-enter-from,
-.appear-leave-to {
-  opacity: 0;
-}
-
-.shake {
-  animation: shake 0.82s cubic-bezier(0.36, 0.07, 0.19, 0.97) both;
-  transform: translate3d(0, 0, 0);
-}
-
-@keyframes shake {
-  10%,
-  90% {
-    transform: translate3d(-2px, 0, 0);
-  }
-
-  20%,
-  80% {
-    transform: translate3d(4px, 0, 0);
-  }
-
-  30%,
-  50%,
-  70% {
-    transform: translate3d(-8px, 0, 0);
-  }
-
-  40%,
-  60% {
-    transform: translate3d(8px, 0, 0);
-  }
-}
-</style>
---
+++ .vitepress/theme/components/AnimatedTitle.vue
@@ -0,0 +1,26 @@
+<template>
+  <a class="text-xl" href="/" aria-label="Site de Nirina Rabeson">
+    ✨
+    <Transition name="title-apparaitre">
+      <span v-if="!show" class="hidden md:inline dark:text-white"
+        >Nirina Rabeson</span
+      >
+    </Transition>
+  </a>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  show: boolean
+}>()
+</script>
+
+<style scoped>
+.title-apparaitre-enter-active {
+  transition: opacity 0.5s ease;
+}
+
+.title-apparaitre-enter-from {
+  opacity: 0;
+}
+</style>
