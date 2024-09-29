984d392e1a3a6f8c4d1269d92094d522bfe594a7
Nirina Rabeson <hello@nirinarabeson.fr>
2024-09-29 10:40:15 +0200

    fix: csp

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -15,5 +15,12 @@ export default defineConfig({
         href: `data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>✨</text></svg>`
       }
     ]
-  ]
+  ],
+  appearance: false,
+  metaChunk: true,
+  vite: {
+    build: {
+      assetsInlineLimit: 0
+    }
+  }
 })
