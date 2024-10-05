454267afda4048418540a4489934e5ece11c882e
Nirina Rabeson <hello@nirinarabeson.fr>
2024-10-05 13:50:03 +0000

    chore: better fetch

--- components/useFetchBalances.ts
+++ components/useFetchBalances.ts
@@ -8,12 +8,12 @@ interface BalanceSolutionResponse {
 const SOLVE_API = import.meta.env.DEV ? 'http://localhost:5328/api/v2/solve' : `/api/v2/solve`
 
 export async function fetchBalances(balances: MaybeRef<number[]>) {
-  const response = await fetch(SOLVE_API, {
+  const { data } = await useFetch<BalanceSolutionResponse>(SOLVE_API, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
     },
     body: JSON.stringify({ balances: toValue(balances) })
   })
-  return (await response.json()) as BalanceSolutionResponse
+  return data.value
 }
