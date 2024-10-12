c8085b22fbdb9d4f850df0b343a7c800cc957ffc
Nirina Rabeson <hello@nirinarabeson.fr>
2024-10-12 16:01:02 +0200

    refactor: composable

--- components/BalancesForm.vue
+++ components/BalancesForm.vue
@@ -1,43 +1,22 @@
 <script setup lang="ts">
-import { useAsyncState, useDebounceFn } from '@vueuse/core'
-import { ref, watch } from 'vue'
-
 import BalanceInput from '@/components/BalanceInput.vue'
 import AjoutDepenseFormulaire from './AjoutDepenseFormulaire.vue'
 import HistoriqueDepenses from './HistoriqueDepenses.vue'
 import StyledButton from './StyledButton.vue'
-import { useAjouterDepense } from './useAjouterDepense'
-import { useBalances } from './useBalances'
-import { fetchBalances } from './useFetchBalances'
 import AffichageRemboursementsV2 from './v2/AffichageRemboursementsV2.vue'
 
-const { balances, nomsBalances, erreurBalance, addBalance } = useBalances()
-const { indexDepenseur, montant, bénéficiaires, ajouterDepense, historiqueDépenses } =
-  useAjouterDepense(balances)
-
-const matriceDeRemboursements = ref<number[][]>([])
-async function _solveBalances() {
-  if (Math.abs(erreurBalance.value) < 0.0001)
-    try {
-      matriceDeRemboursements.value = []
-      const solution = await fetchBalances(balances)
-      matriceDeRemboursements.value = solution?.result_matrix ?? []
-    } finally {
-      //
-    }
-}
-
-const solveBalances = useDebounceFn(_solveBalances, 1000)
-
-const { isLoading, execute } = useAsyncState(solveBalances, undefined, { immediate: true })
-
-watch(
+const {
   balances,
-  async () => {
-    await execute()
-  },
-  { deep: true }
-)
+  nomsBalances,
+  indexDepenseur,
+  montant,
+  bénéficiaires,
+  ajouterDepense,
+  addBalance,
+  isLoading,
+  historiqueDépenses,
+  matriceDeRemboursements
+} = useRemboursementsDetailles()
 </script>
 
 <template>
---
+++ composables/useRemboursementsDetailles.ts
@@ -0,0 +1,48 @@
+import { useDebounceFn, useAsyncState } from '@vueuse/core'
+import { useAjouterDepense } from '~/components/useAjouterDepense'
+import { useBalances } from '~/components/useBalances'
+import { fetchBalances } from '~/components/useFetchBalances'
+
+export function useRemboursementsDetailles() {
+  const { balances, nomsBalances, erreurBalance, addBalance } = useBalances()
+  const { indexDepenseur, montant, bénéficiaires, ajouterDepense, historiqueDépenses } =
+    useAjouterDepense(balances)
+
+  const matriceDeRemboursements = ref<number[][]>([])
+  async function _solveBalances() {
+    if (Math.abs(erreurBalance.value) < 0.0001)
+      try {
+        matriceDeRemboursements.value = []
+        const solution = await fetchBalances(balances)
+        matriceDeRemboursements.value = solution?.result_matrix ?? []
+      } finally {
+        //
+      }
+  }
+
+  const solveBalances = useDebounceFn(_solveBalances, 1000)
+
+  const { isLoading, execute } = useAsyncState(solveBalances, undefined, { immediate: true })
+
+  watch(
+    balances,
+    async () => {
+      await execute()
+    },
+    { deep: true }
+  )
+
+  return {
+    isLoading,
+    balances,
+    addBalance,
+    matriceDeRemboursements,
+    indexDepenseur,
+    montant,
+    bénéficiaires,
+    nomsBalances,
+    erreurBalance,
+    historiqueDépenses,
+    ajouterDepense
+  }
+}
