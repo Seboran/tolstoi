9e81967319a600f4112f1547cfcd58b7d8ca0d43
Nirina Rabeson <hello@nirinarabeson.fr>
2024-10-20 20:25:07 +0200

    chore: ajout macos

---
+++ macos/nirinapass/nirinapassTests/nirinapassTests.swift
@@ -0,0 +1,17 @@
+//
+//  nirinapassTests.swift
+//  nirinapassTests
+//
+//  Created by Nirina Rabeson on 20/10/2024.
+//
+
+import Testing
+@testable import nirinapass
+
+struct nirinapassTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
