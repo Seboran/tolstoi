f44250c28a248755f11fa71986a39d27c5410956
Nirina Rabeson <hello@nirinarabeson.fr>
2024-10-27 17:20:06 +0100

    feat: tentative card

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -1,11 +1,34 @@
 import { defineConfig } from 'vitepress'
 
+const titleCard = 'Blog de Nirina Rabeson'
+const descriptionCard = 'homepage'
+const imageurlCard = 'https://www.nirinarabeson.fr/card.png'
 export default defineConfig({
   title: 'Nirina Rabeson',
   cleanUrls: true,
   head: [
     ['meta', { name: 'twitter:site', content: '@nirinarabeson.fr' }],
     ['meta', { name: 'twitter:card', content: 'summary' }],
+    ['meta', { name: 'twitter:title', content: titleCard }],
+    ['meta', { name: 'og:title', content: titleCard }],
+    [
+      'meta',
+      {
+        name: 'twitter:image',
+        content: imageurlCard
+      }
+    ],
+    [
+      'meta',
+      {
+        name: 'og:image',
+        content: imageurlCard
+      }
+    ],
+    ['meta', { name: 'og:type', content: 'homepage' }],
+    ['meta', { name: 'twitter:description', content: descriptionCard }],
+    ['meta', { name: 'og:description', content: descriptionCard }],
+    ['meta', { name: 'og:url', content: 'https://www.nirinarabeson.fr' }],
     ['description', { content: 'Le site de Nirina Rabeson' }],
     [
       'link',
@@ -19,7 +42,6 @@ export default defineConfig({
   description: 'Site web et blog de Nirina Rabeson',
   appearance: false,
   metaChunk: true,
-  mpa: true,
   vite: {
     build: {
       assetsInlineLimit: 0
