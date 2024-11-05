469f7717fd7b55a178612f4e709a7bd4a557a772
Nirina Rabeson <hello@nirinarabeson.fr>
2024-11-05 23:49:17 +0100

    feat: récupère en temps réel les mots écrits
    problème : je fais n'importe quoi avec les mots avec des accents comme â qui s'écrit en deux temps sur un clavier mac

--- apps/clavier-turbo-rapide/app/InputMotsAEcrire.tsx
+++ apps/clavier-turbo-rapide/app/InputMotsAEcrire.tsx
@@ -1,29 +1,47 @@
 'use client'
-import { useState, type KeyboardEvent } from 'react'
+import { ChangeEvent, useState } from 'react'
 
 export function InputMotsAEcrire() {
+  const mots = ['amour', 'gloire', 'beauté', 'pâtée', 'champignons', 'brun']
+
   const [textCopie, setTextCopié] = useState('')
   const [derniereLettre, setDerniereLettre] = useState('')
 
-  function filterKeyDownAndSetDerniereLettre(
-    e: KeyboardEvent<HTMLInputElement>,
-  ) {
-    if (!e.ctrlKey && !e.metaKey && !e.altKey && e.which != 8) {
-      setDerniereLettre(e.key)
+  const [showErreur, setShowErreur] = useState(false)
+
+  function filterKeyDownAndSetDerniereLettre(e: ChangeEvent<HTMLInputElement>) {
+    const motEcrit = e.target.value
+    if (
+      mots
+        .join(' ')
+        .at(motEcrit.length - 1)
+        ?.toLowerCase() === (motEcrit.at(-1) ?? '').toLowerCase()
+    ) {
+      setShowErreur(false)
+      setTextCopié(motEcrit)
+    } else {
+      setShowErreur(true)
     }
+    setDerniereLettre(motEcrit.at(-1) ?? '')
   }
   return (
     <>
       <label>
         Votre saisie
+        <div className="flex flex-row gap-2">
+          {mots.map((mot) => (
+            <div key={mot}>{mot}</div>
+          ))}
+        </div>
         <input
           name="zone-text"
-          onChange={(e) => setTextCopié(e.target.value)}
-          onKeyDown={(e) => filterKeyDownAndSetDerniereLettre(e)}
+          value={textCopie}
+          onChange={(e) => filterKeyDownAndSetDerniereLettre(e)}
           className="w-full text-black"
         />
-        <div>{textCopie}</div>
-        <div>{derniereLettre}</div>
+        <div>Texte copié :{textCopie}</div>
+        <div>Dernière lettre : {derniereLettre}</div>
+        {showErreur ? <div>Erreur de saisie !</div> : null}
       </label>
     </>
   )
--- apps/clavier-turbo-rapide/app/page.tsx
+++ apps/clavier-turbo-rapide/app/page.tsx
@@ -1,7 +1,6 @@
 import { InputMotsAEcrire } from './InputMotsAEcrire'
 
 export default function Home() {
-  const mots = ['amour', 'gloire', 'beauté', 'pâtée', 'champignons', 'brun']
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start max-w-[80ch]">
@@ -14,11 +13,6 @@ export default function Home() {
           <div>
             <h3>Liste de mots</h3>
             <div>Temps restant: TODO</div>
-            <div className="flex flex-row gap-2">
-              {mots.map((mot) => (
-                <div key={mot}>{mot}</div>
-              ))}
-            </div>
           </div>
           <InputMotsAEcrire />
         </section>
