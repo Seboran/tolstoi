bc86009d0233aa10c737d484d819be7602c2562a
Nirina Rabeson <hello@nirinarabeson.fr>
2024-11-16 21:57:47 +0100

    chore: ajout des dates, des liens vers les autres articles

--- apps/nirina-site/astro.config.ts
+++ apps/nirina-site/astro.config.ts
@@ -5,16 +5,9 @@ import tailwind from '@astrojs/tailwind'
 
 import vercel from '@astrojs/vercel/serverless'
 
-import starlight from '@astrojs/starlight'
-
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind(),
-    starlight({
-      title: 'Site de Nirina Rabeson',
-    }),
-  ],
+  integrations: [tailwind()],
   site: 'https://www.nirinarabeson.fr',
   output: 'server',
   adapter: vercel({
