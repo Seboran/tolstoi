19f5e8965a6873ea2aa390cbba889eb76528ecfb
Nirina Rabeson <hello@nirinarabeson.fr>
2024-11-16 20:24:34 +0100

    chore: deploy vercel

--- apps/nirina-site/astro.config.ts
+++ apps/nirina-site/astro.config.ts
@@ -3,8 +3,14 @@ import { defineConfig } from 'astro/config'
 
 import tailwind from '@astrojs/tailwind'
 
+import vercel from '@astrojs/vercel/serverless'
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   site: 'https://www.nirinarabeson.fr',
+  output: 'server',
+  adapter: vercel({
+    isr: true,
+  }),
 })
