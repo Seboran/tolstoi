2d9cfce4bbd4a11fd4f5272ada07a85a0ef8ff1f
Nirina Rabeson <hello@nirinarabeson.fr>
2024-11-17 11:14:19 +0100

    chore: design, cleanup, mdx

--- apps/nirina-site/astro.config.ts
+++ apps/nirina-site/astro.config.ts
@@ -5,9 +5,11 @@ import tailwind from '@astrojs/tailwind'
 
 import vercel from '@astrojs/vercel/serverless'
 
+import mdx from '@astrojs/mdx';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), mdx()],
   site: 'https://www.nirinarabeson.fr',
   output: 'hybrid',
   adapter: vercel({
