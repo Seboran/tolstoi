d9e383a71f651108ed67941a9c4b4d3f6cbfa6f3
Nirina Rabeson <hello@nirinarabeson.fr>
2024-11-17 15:04:39 +0100

    chore: netlify

--- apps/nirina-site/astro.config.ts
+++ apps/nirina-site/astro.config.ts
@@ -3,16 +3,16 @@ import { defineConfig } from 'astro/config'
 
 import tailwind from '@astrojs/tailwind'
 
-import vercel from '@astrojs/vercel/serverless'
+import mdx from '@astrojs/mdx'
 
-import mdx from '@astrojs/mdx';
+import netlify from '@astrojs/netlify'
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), mdx()],
   site: 'https://www.nirinarabeson.fr',
   output: 'hybrid',
-  adapter: vercel({
-    isr: true,
+  adapter: netlify({
+    edgeMiddleware: true,
   }),
 })
