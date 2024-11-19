dee35b965a7a5fdb0204a1268aff88c6cd95e0f8
Nirina Rabeson <hello@nirinarabeson.fr>
2024-11-19 21:16:01 +0100

    fix: ne pas publier les drafts

--- apps/nirina-site/src/pages/rss.xml.js
+++ apps/nirina-site/src/pages/rss.xml.js
@@ -2,7 +2,7 @@ import rss from '@astrojs/rss'
 import { getCollection } from 'astro:content'
 
 export async function GET(context) {
-  const blog = await getCollection('posts')
+  const blog = (await getCollection('posts')).filter(({ data }) => !data.draft)
   return rss({
     // `<title>` field in output xml
     title: 'Le blog de Nirina Rabeson',
