f757e0ccfa4ee456c2aad57419c2e817664044de
Nirina Rabeson <hello@nirinarabeson.fr>
2024-11-20 15:59:06 +0000

    chore: goodbye vitepress

--- apps/blog/.vitepress/theme/Contact.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <section>
-    <Content class="prose dark:prose-invert max-w-none pt-10 pb-8"/>
-  </section>
-</template>
-
-<script setup lang="ts"></script>
-
-<style scoped></style>
