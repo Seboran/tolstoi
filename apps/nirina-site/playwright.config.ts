fc95d8fea60ea2b48c83b152b3dd1c0c4f7274cf
Nirina Rabeson <hello@nirinarabeson.fr>
2024-12-01 17:40:15 +0100

    chore: oups

--- apps/nirina-site/playwright.config.ts
+++ apps/nirina-site/playwright.config.ts
@@ -26,7 +26,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: 'http://localhost:8888',
+    baseURL: 'http://localhost:4321',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
@@ -72,8 +72,8 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'pnpm netlify serve',
-    url: 'http://localhost:8888',
+    command: 'pnpm dev',
+    url: 'http://localhost:4321',
     reuseExistingServer: !process.env.CI,
   },
 })
