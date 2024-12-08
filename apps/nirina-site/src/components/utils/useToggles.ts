d08845d1521e56bd5d95fadb5831817f73e15eba
Nirina Rabeson <hello@nirinarabeson.fr>
2024-12-08 17:12:49 +0100

    chore(nirina-site): moved toggle to edge function

--- apps/nirina-site/src/components/utils/useToggles.ts
+++ apps/nirina-site/src/components/utils/useToggles.ts
@@ -1,7 +1,15 @@
 import { ENABLE_CHAT } from '../../../utils/environment-variables'
 
-export function useToggles() {
+export async function useToggles(togglesEndpoint: URL) {
+  let body: Record<string, unknown> = {}
+  try {
+    const request = await fetch(togglesEndpoint)
+    body = await request.json()
+  } catch (e) {
+    console.error(e)
+  }
+
   return {
-    enableChatWithMistral: !!process.env[ENABLE_CHAT],
+    enableChatWithMistral: !!body[ENABLE_CHAT],
   }
 }
