7ae120f68e65816a59f673c08baa4011fe064b0f
Nirina Rabeson <hello@nirinarabeson.fr>
2025-01-01 14:51:57 +0100

    chore(nirina-site): regroupe l'API de collections en un seul point

---
+++ apps/nirina-site/src/utils/getCollections.ts
@@ -0,0 +1,9 @@
+import { getCollection } from 'astro:content'
+
+export async function getPublishedPosts() {
+  return await getCollection('posts', ({ data }) => !data.draft || import.meta.env.DEV)
+}
+
+export async function getSortedPublishedPosts() {
+  return (await getPublishedPosts()).sort((a, b) => b.data.date.getTime() - a.data.date.getTime())
+}
