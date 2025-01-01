0f649fee6e28e8bd4373ed0b0412ed39a52c7e09
Nirina Rabeson <hello@nirinarabeson.fr>
2025-01-01 14:52:24 +0100

    fix(nirina-site): répare les URL dans les flux RSS

--- apps/nirina-site/src/pages/rss.xml.js
+++ apps/nirina-site/src/pages/rss.xml.js
@@ -1,8 +1,8 @@
 import rss from '@astrojs/rss'
-import { getCollection } from 'astro:content'
+import { getSortedPublishedPosts } from '../utils/getCollections'
 
 export async function GET(context) {
-  const blog = (await getCollection('posts')).filter(({ data }) => !data.draft)
+  const blog = await getSortedPublishedPosts()
   return rss({
     // `<title>` field in output xml
     title: 'Le blog de Nirina Rabeson',
@@ -20,7 +20,7 @@ export async function GET(context) {
       description: post.data.excerpt,
       // Compute RSS link from post `slug`
       // This example assumes all posts are rendered as `/blog/[slug]` routes
-      link: `/posts/${post.slug}/`,
+      link: `/posts/${post.id}/`,
     })),
     // (optional) inject custom xml
     customData: `<language>fr-fr</language>`,
