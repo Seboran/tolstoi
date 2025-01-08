b8f1788c4c61db0c009a808b29a068abb4f18536
Nirina Rabeson <hello@nirinarabeson.fr>
2025-01-08 19:17:31 +0100

    chore(bonne-energie): init nouveau projet

---
+++ apps/bonne-energie-bons-voisins/app/app.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <h1>Bonne énergie bons voisins</h1>
+    <h2>
+      Le but de ce projet est de représenter la distribution d'énergie entre plusieurs entités
+    </h2>
+  </div>
+</template>
