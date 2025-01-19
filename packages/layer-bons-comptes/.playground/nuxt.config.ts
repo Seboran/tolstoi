0791a22921dabecc6aaefd52eea339ccb3bd4104
Nirina Rabeson <hello@nirinarabeson.fr>
2025-01-19 21:45:40 +0100

    chore: remove eslint and prettier

--- packages/layer-bons-comptes/.playground/nuxt.config.ts
+++ packages/layer-bons-comptes/.playground/nuxt.config.ts
@@ -2,7 +2,6 @@ import { fileURLToPath } from 'node:url'
 
 export default defineNuxtConfig({
   extends: ['..'],
-  modules: ['@nuxt/eslint'],
   eslint: {
     config: {
       // Use the generated ESLint config for lint root project as well
