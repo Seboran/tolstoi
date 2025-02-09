4e6c30c82a326a868670cab9a467562041fbd0b3
Nirina Rabeson <hello@nirinarabeson.fr>
2025-02-09 17:34:50 +0100

    chore(freelance): cd

--- apps/freelance-simulateur/next.config.mjs
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export', // Génère une application Single-Page Application (SPA).
-  distDir: './dist', // Change le répertoire de sortie vers `./dist/`.
-}
-
-export default nextConfig
