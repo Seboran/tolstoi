7a2d9e6c4b017945d9a5aa393dd8477bccc80cf6
Nirina Rabeson <hello@nirinarabeson.fr>
2025-03-16 15:26:16 +0100

    feat(cemantix-visualiseur): calcul liste de mots

--- apps/cemantix-visualiseur/src/components/CemantixVisualizer.vue
+++ apps/cemantix-visualiseur/src/components/CemantixVisualizer.vue
@@ -1,13 +1,14 @@
 <script setup lang="ts">
+import { listeMotsInitiale } from './listeMots'
 import DisplayWordsCanvas from './words-entry/DisplayWordsCanvas.vue'
 import ListWordsTextArea from './words-entry/ListWordsTextArea.vue'
 
 import { ref } from 'vue'
 
-const words = ref<string[]>(['courage', 'désespoir', 'tristesse'])
+const words = ref<string[]>(listeMotsInitiale)
 </script>
 
 <template>
-  <ListWordsTextArea v-model="words" />
+  <ListWordsTextArea class="right-0" v-model="words" />
   <DisplayWordsCanvas :words />
 </template>
--- apps/cemantix-visualiseur/src/components/words-entry/useWordToPosition.ts
+++ apps/cemantix-visualiseur/src/components/words-entry/useWordToPosition.ts
@@ -8,11 +8,29 @@ export interface WordPosition {
     y: number
   }
 }
+
+const NORMALIZATION_FACTOR = 90
+
 export function useWordToPosition(words: Ref<string[]>) {
   const positions = ref<WordPosition[]>([])
-  watch(words, async () => {
-    positions.value = await convertWords(words.value)
-  })
+  watch(
+    words,
+    async () => {
+      const trimedWords = words.value.map((w) => w.trim())
+      const positionsToNormalize = await convertWords(trimedWords)
+      const maxX = Math.max(...positionsToNormalize.map((p) => Math.abs(p.position.x)))
+      const maxY = Math.max(...positionsToNormalize.map((p) => Math.abs(p.position.y)))
+      positionsToNormalize.forEach((p) => {
+        p.position.x /= maxX
+        p.position.y /= maxY
+        // Normalize to 100
+        p.position.x *= NORMALIZATION_FACTOR
+        p.position.y *= NORMALIZATION_FACTOR
+      })
+      positions.value = positionsToNormalize
+    },
+    { immediate: true },
+  )
   return {
     positions,
   }
