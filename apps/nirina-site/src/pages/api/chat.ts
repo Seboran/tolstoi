5329f594198567af80184ab7c93905b85c6e3f8c
Nirina Rabeson <hello@nirinarabeson.fr>
2025-04-02 11:25:12 +0200

    feat(site): node adapter

---
+++ apps/nirina-site/src/pages/api/chat.ts
@@ -0,0 +1,20 @@
+import { getSecret } from 'astro:env/server'
+import type { APIRoute } from 'astro'
+import {
+  ENABLE_CHAT as ENABLE_CHAT_KEY,
+  MISTRAL_AGENT_ID_KEY,
+  MISTRAL_API_ENDPOINT_KEY,
+  MISTRAL_API_KEY,
+} from '../../../utils/environment-variables'
+import { useChatFunction } from '../../server-functions/useChatFunction'
+const { post } = useChatFunction({
+  apiKey: getSecret(MISTRAL_API_KEY),
+  MISTRAL_AGENT_ID: getSecret(MISTRAL_AGENT_ID_KEY),
+  ENABLE_CHAT: getSecret(ENABLE_CHAT_KEY),
+  MISTRAL_API_ENDPOINT: getSecret(MISTRAL_API_ENDPOINT_KEY),
+})
+export const POST: APIRoute = async ({ request }) => {
+  return await post(request)
+}
+
+export const prerender = false
