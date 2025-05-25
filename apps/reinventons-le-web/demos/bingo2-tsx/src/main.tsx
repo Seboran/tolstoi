90de6a1e1157d1b2c26fd0aa7ce8302d814a8275
Nirina Rabeson <hello@nirinarabeson.fr>
2025-05-25 19:38:08 +0200

    feat: add StatusMessage component to display status messages

--- apps/reinventons-le-web/demos/bingo2-tsx/src/main.tsx
+++ apps/reinventons-le-web/demos/bingo2-tsx/src/main.tsx
@@ -3,6 +3,7 @@ import { AppContainer } from './components/AppContainer'
 import { BingoTableau } from './components/BingoTableau'
 import { Button } from './components/Button'
 import { NumberInput } from './components/NumberInput'
+import { StatusMessage } from './components/StatusMessage'
 import { reactive, reactiveSet } from './framework/reactive'
 import { mountReactive } from './framework/renderer'
 import { h } from './framework/vdom'
@@ -37,6 +38,7 @@ mountReactive(() => {
   console.log('Render function called')
   return (
     <AppContainer>
+      <StatusMessage message="This component uses the old h() function!" />
       <NumberInput
         value={state.inputValue}
         placeholder="Enter number"
