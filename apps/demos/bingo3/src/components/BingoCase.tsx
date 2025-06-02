500f0f3c6c7c8fbc62cbc0680d623622c89a477d
Nirina Rabeson <hello@nirinarabeson.fr>
2025-06-02 22:20:35 +0200

    chore(web): pour préparer le live coding

--- apps/demos/bingo3/src/components/BingoCase.tsx
+++ apps/demos/bingo3/src/components/BingoCase.tsx
@@ -1,20 +1,13 @@
 import type { Component } from '../bingo2'
-import { reactive } from '../framework/reactive'
 import { h } from '../framework/vdom'
 
 export const BingoCase: Component<{ value: number }> = ({ value }) => {
-  // Each cell has its own reactive state
-  const cellState = reactive({
-    isClicked: false,
-  })
-
   function handleClick() {
-    cellState.isClicked = !cellState.isClicked
+    // TODO: Implement logic to handle click
   }
 
   return (
-    <td class={() => (cellState.isClicked ? 'marked' : undefined)} onclick={handleClick}>
-      {String(value)}
-    </td>
+    // class={() => (cellState.isClicked ? 'marked' : undefined)}
+    <td onclick={handleClick}>{String(value)}</td>
   )
 }
