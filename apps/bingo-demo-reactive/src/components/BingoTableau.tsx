75b9ae3c98e313f41dcee1041570ce4e0c4f9cf5
Nirina Rabeson <hello@nirinarabeson.fr>
2025-06-07 12:17:10 +0200

    chore: ne pas péter le build de préférence

--- apps/bingo-demo-reactive/src/components/BingoTableau.tsx
+++ apps/bingo-demo-reactive/src/components/BingoTableau.tsx
@@ -4,8 +4,8 @@ import { BingoRow } from './BingoRow'
 
 export const BingoTableau: Component<{ rows: number[][] }> = ({ rows }) => (
   <table class="bingo-tableau">
-    {rows.map((r, index) => (
-      <BingoRow key={index} values={r} />
+    {rows.map((r) => (
+      <BingoRow values={r} />
     ))}
   </table>
 )
