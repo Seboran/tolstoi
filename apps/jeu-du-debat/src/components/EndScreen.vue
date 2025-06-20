92ee6ab0a844e4576b4201bec1796e80d574d41b
Nirina Rabeson <hello@nirinarabeson.fr>
2025-06-20 22:16:16 +0200

    chore: mise à la retraite du jeu du débat

--- apps/jeu-du-debat/src/components/EndScreen.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts">
-const emits = defineEmits<{ (e: 'replay'): void }>()
-</script>
-
-<template>
-  <div><!-- empty div --></div>
-  <h2>Débattez !</h2>
-  <button class="btn btn-primary w-100" @click="emits('replay')">Rejouer ?</button>
-</template>
