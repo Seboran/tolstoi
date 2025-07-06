ca5d5c981f6d9daa113c5949bd6ef2b51adec3a3
Nirina Rabeson <hello@nirinarabeson.fr>
2025-07-06 22:23:35 +0200

    fix(site): improve markdownX parsing

--- apps/nirina-site/src/utils/getExcerptFromMarkdown.ts
+++ apps/nirina-site/src/utils/getExcerptFromMarkdown.ts
@@ -5,13 +5,20 @@ export function getExcerptFromMarkdown(content: string): string {
   let [excerpt] = body.split('\n---')
 
   // Remove images: ![alt](url) or ![](url)
-  excerpt = excerpt.replace(/!\[[^\]]*\]\([^\)]*\)/g, '')
+  excerpt = excerpt.replace(/!\[[^\]]*\]\([^)]*\)/g, '')
 
   // Remove footnote references: [^1], [^note], etc.
   excerpt = excerpt.replace(/\[\^[^\]]+\]/g, '')
 
   // Remove footnote definitions: lines starting with [^1]:
   excerpt = excerpt.replace(/^\[\^[^\]]+\]:.*$/gm, '')
+
+  // Remove MDX import lines
+  excerpt = excerpt.replace(/^import\s.+from\s.+;?$/gm, '')
+
+  // Remove MDX/JSX components: <Component ... /> or <Component></Component>
+  excerpt = excerpt.replace(/^<([A-Z][A-Za-z0-9]*)\b[^>]*\/>$/gm, '') // self-closing
+  excerpt = excerpt.replace(/^<([A-Z][A-Za-z0-9]*)\b[^>]*>.*?<\/\1>$/gms, '') // block
 
   return excerpt.trim()
 }
